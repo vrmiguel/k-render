55eef9f78b76fce009989fb4621e46163846b4a2
V. R. Miguel <36349314+vrmiguel@users.noreply.github.com>
2020-06-22 12:28:53 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,15 @@ int main(int argc, char ** argv) {
     config_t cfg = parse_cli_input(argc, argv);
     Model model = Model(cfg.obj_file);
 
-    //TGAImage wireframe    = draw_wireframe(model, cfg, white); // Draws wireframe
-    //TGAImage   gouraud      = apply_gouraud_shade_no_z_buffer(model, cfg);   // Applies Gouraud shading without z-buffering
+    TGAImage wireframe    = draw_wireframe(model, cfg, white);             // Draws wireframe
+    TGAImage   gouraud    = apply_gouraud_shade_no_z_buffer(model, cfg);   // Applies Gouraud shading without z-buffering
     //TGAImage   z_buffered   = triangle_fill_random_colors(model, cfg);
-    TGAImage  t = apply_gouraud_shade_z_buffer(model, cfg);
+    TGAImage  gouraud_z   = apply_gouraud_shade_z_buffer(model, cfg);      // Applies Gouraud shading with z-buffering
 
 
-    //save_result(wireframe, "output-wireframe.tga");
-    //save_result(gouraud,   "output-gouraud.tga");
+    save_result(wireframe, "output-wireframe.tga");
+    //save_result(gouraud,   "output-gouraud-no-z.tga");
     //save_result(z_buffered,   "output-random-colors.tga");
-    save_result(t,   "output-random-colors.tga");
+    save_result(gouraud_z,   "output-gourand-with-z.tga");
     return 0;
 }
